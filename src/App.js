bd835daf8858989a85b2769b29b697f0c81ce8ea
Argiris Anastasakis <anastasakis.an@gmail.com>
2022-11-01 21:12:11 +0200

    Add Expenses Filter

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
-import Expenses from "./components/Expenses/Expenses";
+import React from "react";
+
 import NewExpense from "./components/NewExpense/NewExpense";
+import Expenses from "./components/Expenses/Expenses";
 
 const App = () => {
   const expenses = [
@@ -25,8 +27,16 @@ const App = () => {
   ];
 
   const addExpenseHandler = (expense) => {
+    console.log("In App.js");
     console.log(expense);
   };
+
+  // return React.createElement(
+  //   'div',
+  //   {},
+  //   React.createElement('h2', {}, "Let's get started!"),
+  //   React.createElement(Expenses, { items: expenses })
+  // );
 
   return (
     <div>
